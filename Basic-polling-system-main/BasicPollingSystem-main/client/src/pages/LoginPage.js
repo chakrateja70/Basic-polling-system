59c06a40356f2341f2ceb3624485c417b57dfd29
teja <tejapolamarasetty2004@gmail.com>
2024-08-15 14:31:50 +0530

    upfayte

--- Basic-polling-system-main/BasicPollingSystem-main/client/src/pages/LoginPage.js
+++ Basic-polling-system-main/BasicPollingSystem-main/client/src/pages/LoginPage.js
@@ -27,7 +27,7 @@ export const LoginPage = () => {
 
 
     try {
-      const response = await fetch(`https://basic-polling-system.vercel.app/api/${role}/login`, {
+      const response = await fetch(`api/${role}/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
